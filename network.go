11daadf6ea91d79226287a391a8a6149cff2239c
Harrison <harrison@theshoebridges.com>
2016-09-10 23:30:51 +1000

    Add peek function

--- network.go
+++ network.go
@@ -19,37 +19,22 @@ type Networker struct {
 func (n *Networker) Handle(conn net.Conn) {
 	defer conn.Close()
 
-	buf := &bytes.Buffer{}
-	_, err := buf.ReadFrom(conn)
+	com, buf, err := n.peek(conn)
 	if err != nil {
-		log.Println("Unable to read properly:", err)
+		log.Println("Could not peek command:", err)
 		return
 	}
 
-	cmd := bytes.NewBuffer(buf.Bytes())
-	if err != nil {
-		log.Println("Unable to copy:", err)
-		return
+	if com.Command == "connection" {
+		err = n.connection(buf, conn)
+		if err != nil {
+			log.Println("Unable to sustain connection command:", err)
+			return
+		}
 	}
 
-	com := Communication{}
-
-	err = json.NewDecoder(cmd).Decode(&com)
-	if err != nil {
-		log.Println("Unable to decode JSON:", err)
-		return
-	}
-
-	if com.Command != "connection" {
-		fmt.Println("Unknown command:", com.Command)
-		return
-	}
-
-	err = n.connection(buf, conn)
-	if err != nil {
-		log.Println("Unable to sustain connection command:", err)
-		return
-	}
+	log.Println("Unknown command:", com.Command)
+	return
 }
 
 func (n *Networker) connection(buf *bytes.Buffer, conn net.Conn) error {
@@ -63,4 +48,24 @@ func (n *Networker) connection(buf *bytes.Buffer, conn net.Conn) error {
 	conn.Write([]byte(fmt.Sprintf("Hello, %v!", c.Username)))
 
 	return nil
+}
+
+func (n *Networker) peek(conn net.Conn) (Communication, *bytes.Buffer, error) {
+	buf := &bytes.Buffer{}
+	cmd := &bytes.Buffer{}
+	com := Communication{}
+
+	_, err := buf.ReadFrom(conn)
+	if err != nil {
+		return com, cmd, err
+	}
+
+	cmd = bytes.NewBuffer(buf.Bytes())
+
+	err = json.NewDecoder(cmd).Decode(&com)
+	if err != nil {
+		return com, cmd, err
+	}
+
+	return com, cmd, nil
 }
